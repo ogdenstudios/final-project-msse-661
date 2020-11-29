3ffa94202e34e52aeac030854c2488d973f595b5
Tyler Williams <tyler@ogdenstudios.xyz>
2020-11-29 11:23:57 -0700

    get tests working for grading of incorrect and partial answers

--- api/src/lib/grade/grade.js
+++ api/src/lib/grade/grade.js
@@ -1,12 +1,17 @@
 const Grade = (payload) => {
+  const potOddsAnswer = payload.potOdds
+  const handStrengthAnswer = payload.handStrength
   const pot = payload.scenario.bettingInformation.pot
   const callAmount = Math.max(
     ...payload.scenario.bettingInformation.currentBettingRound
   )
   const finalPotSize = pot + callAmount
-  if (finalPotSize) {
-    return 0
+  const actualPotOdds = Math.round((callAmount / finalPotSize) * 100)
+  let grade = 0
+  if (actualPotOdds === potOddsAnswer) {
+    grade += 0.5
   }
+  return grade
 }
 
 export default Grade
--- api/src/lib/grade/grade.test.js
+++ api/src/lib/grade/grade.test.js
@@ -2,12 +2,30 @@ import Grade from './grade'
 
 describe('Betting', () => {
   it('Returns a 0 if no part of the equation is correct', () => {
-    const wrongAnswer = {}
+    const wrongAnswer = {
+      potOdds: 30,
+      handStrength: 30,
+      scenario: {
+        bettingInformation: {
+          pot: 100,
+          currentBettingRound: [30],
+        },
+      },
+    }
     expect(Grade(wrongAnswer)).toEqual(0)
   })
 
   it('Returns a 0.5 if the pot odds are correct but the hand strength is not', () => {
-    const partialAnswer = {}
+    const partialAnswer = {
+      potOdds: 30,
+      handStrength: 30,
+      scenario: {
+        bettingInformation: {
+          pot: 70,
+          currentBettingRound: [30],
+        },
+      },
+    }
     expect(Grade(partialAnswer)).toEqual(0.5)
   })
 
